6d9fd4a0f329138e2e8f3627f2e11db34250ada1
hmc <hmc@git.com>
2020-02-28 18:59:23 +0800

    day05favicon

---
+++ Springboot/springboot-05day/src/main/java/com/hmc/springboot05day/Springboot05dayApplication.java
@@ -0,0 +1,13 @@
+package com.hmc.springboot05day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot05dayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot05dayApplication.class, args);
+    }
+
+}
